a9f44bd790834ab5f3667a2bef2c11fd67bd8fe1
Nitesh Kamat <nitesh0434@gmail.com>
2026-02-02 12:03:24 +0545

    remove the relative number

--- lua/nitesh/options.lua
+++ lua/nitesh/options.lua
@@ -1,6 +1,6 @@
 -- Turn on line numbers
 vim.opt.number = true
-vim.opt.relativenumber = true      -- Show relative numbers
+--vim.opt.relativenumber = true      -- Show relative numbers
 vim.opt.tabstop = 4                -- A tab = 4 spaces
 vim.opt.shiftwidth = 4             -- Indent = 4 spaces
 vim.opt.expandtab = true           -- Use spaces instead of tabs
